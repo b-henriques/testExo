38aaee4fe3a92a5bcd7f81bfb7c7af40b537f882
b-henriques <75887092+b-henriques@users.noreply.github.com>
2021-02-09 14:05:51 +0100

    first commit

--- ProjGit.java
+++ ProjGit.java
@@ -1,6 +1,8 @@
 public class ProjGit{
     public static void main(String[] args) {
         System.out.println("NOM PROJET");
-	
+	for (String s : args) {
+                System.out.println(s);
+
     }
 }
